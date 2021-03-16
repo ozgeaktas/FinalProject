2c30bf0d4f689007e2334c539bf9c67502f8e8b6
ozgeaktas <54955167+ozgeaktas@users.noreply.github.com>
2021-03-16 13:03:11 +0300

    10.gün ders sonu

---
+++ Core/Utilities/Results/IResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    //Temel voidler için başlangıç
+    public interface IResult
+    {
+        bool Success { get; }//yapmaya çalıştığn işlem başarılı true...
+        string Message { get; }//işlem başarılıdır yazar mesela...(yönlendirme) 
+    }
+}
